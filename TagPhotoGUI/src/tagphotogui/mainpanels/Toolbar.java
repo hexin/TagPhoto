d5f027a33a6c711002317f79e72d41e8d9b3c6d5
takijeden <pawelcincio@gmail.com>
2012-05-09 00:27:33 +0200

    Próby wsadzania obrazka.

--- TagPhotoGUI/src/tagphotogui/mainpanels/Toolbar.java
+++ TagPhotoGUI/src/tagphotogui/mainpanels/Toolbar.java
@@ -1,8 +1,19 @@
 package tagphotogui.mainpanels;
 
+import com.sun.corba.se.spi.orbutil.fsm.Input;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import org.xml.sax.InputSource;
 
 /**
  * @author Paweł Cińcio
@@ -11,16 +22,26 @@ public class Toolbar extends JPanel {
 
     private JPanel leftUp, left, leftDown, up, down, right;
     private Toolbox toolbox;
+    private int margin;
+
+    private class LeftUp extends JPanel {
+
+    }
 
     public Toolbar() {
         this.setLayout(new BorderLayout());
         preparePanels();
+        margin = 45;
 
         leftDown.setBackground(Color.BLUE);
         leftUp.setBackground(Color.BLUE);
     }
 
     private void preparePanels() {
+        Dimension dim = new Dimension(margin, margin);
+        URL url = getClass().getResource("small-smile.gif");
+        ImageIcon icon = new ImageIcon(url);
+
         // At first up and down edges.
         up = new JPanel();
         up.setLayout(new BorderLayout());
@@ -32,9 +53,12 @@ public class Toolbar extends JPanel {
 
         // Add left corners to them
         leftUp = new JPanel();
+        leftUp.add(new JLabel(icon));
+        leftUp.setSize(margin, margin);
         up.add(leftUp, BorderLayout.WEST);
 
         leftDown = new JPanel();
+        leftDown.setSize(margin, margin);
         down.add(leftDown, BorderLayout.WEST);
 
         // Left and right side.
@@ -45,6 +69,7 @@ public class Toolbar extends JPanel {
         this.add(right, BorderLayout.EAST);
 
         toolbox = new Toolbox();
+        toolbox.add(new JLabel(icon));
         this.add(toolbox, BorderLayout.CENTER);
     }
 }
