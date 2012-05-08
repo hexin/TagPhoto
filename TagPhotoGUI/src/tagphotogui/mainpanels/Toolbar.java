2d0a6865fc9730b93a4d560056299d5bf458d086
takijeden <pawelcincio@gmail.com>
2012-05-08 23:26:29 +0200

    Toolbar został podzielony.

--- TagPhotoGUI/src/tagphotogui/mainpanels/Toolbar.java
+++ TagPhotoGUI/src/tagphotogui/mainpanels/Toolbar.java
@@ -1,5 +1,6 @@
 package tagphotogui.mainpanels;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JPanel;
 
@@ -8,8 +9,42 @@ import javax.swing.JPanel;
  */
 public class Toolbar extends JPanel {
 
+    private JPanel leftUp, left, leftDown, up, down, right;
+    private Toolbox toolbox;
+
     public Toolbar() {
-        this.setBackground(Color.BLUE);
+        this.setLayout(new BorderLayout());
+        preparePanels();
+
+        leftDown.setBackground(Color.BLUE);
+        leftUp.setBackground(Color.BLUE);
     }
 
+    private void preparePanels() {
+        // At first up and down edges.
+        up = new JPanel();
+        up.setLayout(new BorderLayout());
+        this.add(up, BorderLayout.NORTH);
+
+        down = new JPanel();
+        down.setLayout(new BorderLayout());
+        this.add(down, BorderLayout.SOUTH);
+
+        // Add left corners to them
+        leftUp = new JPanel();
+        up.add(leftUp, BorderLayout.WEST);
+
+        leftDown = new JPanel();
+        down.add(leftDown, BorderLayout.WEST);
+
+        // Left and right side.
+        left = new JPanel();
+        this.add(left, BorderLayout.WEST);
+
+        right = new JPanel();
+        this.add(right, BorderLayout.EAST);
+
+        toolbox = new Toolbox();
+        this.add(toolbox, BorderLayout.CENTER);
+    }
 }
